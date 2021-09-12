a6a32a184c0a36d25c9ef70aa4eff2ae1f67d821
makindotcc <9150636+makindotcc@users.noreply.github.com>
2021-09-13 00:54:33 +0200

    Fetch textures.txt from lunars api

--- lunarlauncher/cmd/launch.go
+++ lunarlauncher/cmd/launch.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"errors"
 	"log"
 
 	"github.com/makindotcc/lunarlauncher"
 )
+
+func downloadTextures(texturesBaseUrl string, textures []lunarlauncher.TextureMeta) {
+	allTexturesCount := len(textures)
+	for processedCount, texture := range textures {
+		log.Printf("Downloading texture (%d/%d): %s\n", (processedCount + 1), allTexturesCount, texture)
+		err := lunarlauncher.DownloadTexture(texturesBaseUrl, texture)
+		if errors.Is(err, lunarlauncher.ErrAlreadyDownloaded) {
+			log.Printf("Texture %v is already downloaded. Skipping...\n", texture)
+		} else if err != nil {
+			log.Printf("Texture %v download error: %s\n", texture, err)
+		}
+	}
+}
+
+func downloadLaunchTextures(launchMeta lunarlauncher.LaunchMeta) {
+	textures, err := lunarlauncher.FetchTextures(launchMeta.Textures.IndexURL)
+	if err != nil {
+		panic(err)
+	}
+	downloadTextures(launchMeta.Textures.BaseURL, textures)
+}
 
 func main() {
 	log.Println("Preparing lunar assets...")
@@ -15,4 +37,5 @@ func main() {
 	}
 	log.Println("Got launch meta:", launchMeta)
 	
+	downloadLaunchTextures(launchMeta)
 }
--- lunarlauncher/textures.go
+++ lunarlauncher/textures.go
@@ -4,112 +4,48 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"os"
-	"path"
-	"path/filepath"
 	"strings"
 )
 
 const texturesDir = "textures"
-const texturesBaseUrl = "https://textures.lunarclientcdn.com/file/"
 
-var errAlreadyDownloaded = errors.New("already downloaded")
-
-type textureMeta struct {
-	path string
-	hash string
+type TextureMeta struct {
+	Path string
+	Hash string
 }
 
-func isAlreadyDownloaded(downloadPath string) (bool, error) {
-	_, err := os.Stat(downloadPath)
-	if err == nil {
-		// todo: compare hash
-		return true, nil
-	}
-	if !errors.Is(err, os.ErrNotExist) {
-		return false, fmt.Errorf("file stat (%s): %w", downloadPath, err)
-	}
-	return false, nil
+func DownloadTexture(texturesBaseUrl string, meta TextureMeta) error {
+	// meta.path
+	// texturesBaseUrl + meta.hash
+	return downloadFile(meta.Path, texturesBaseUrl+meta.Hash)
 }
 
-func downloadTexture(meta textureMeta) error {
-	downloadPath := path.Join(texturesDir, meta.path)
-	isSubDir, err := IsSubDir(texturesDir, downloadPath)
-	if err != nil {
-		return fmt.Errorf("sub dir validate: %w", err)
-	}
-	if !isSubDir {
-		return fmt.Errorf("texture directory path traversal '%s'", downloadPath)
-	}
-	
-	alreadyDownloaded, err := isAlreadyDownloaded(downloadPath)
-	if err != nil {
-		return fmt.Errorf("is already downloaded: %w", err)
-	}
-	if alreadyDownloaded {
-		return errAlreadyDownloaded
-	}
-
-	response, err := http.Get(texturesBaseUrl + meta.hash)
-	if err != nil {
-		return nil
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("invalid response status code (%d)", response.StatusCode)
-	}
-
-	err = os.MkdirAll(filepath.Dir(downloadPath), os.ModePerm)
-	if err != nil {
-		return fmt.Errorf("mkdir (%s)", downloadPath)
-	}
-	file, err := os.Create(downloadPath)
-	if err != nil {
-		return fmt.Errorf("create file: %w", err)
-	}
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		return fmt.Errorf("write to file from response: %w", err)
-	}
-	return nil
-}
-
-func downloadTextures(textures []textureMeta) {
-	allTexturesCount := len(textures)
-	for processedCount, texture := range textures {
-		log.Printf("Downloading texture (%d/%d): %s\n", (processedCount + 1), allTexturesCount, texture)
-		err := downloadTexture(texture)
-		if errors.Is(err, errAlreadyDownloaded) {
-			log.Printf("Texture %v is already downloaded. Skipping...\n", texture)
-		} else if err != nil {
-			log.Printf("Texture %v download error: %s\n", texture, err)
-		}
-	}
-}
-
-func parseRawTextureMeta(rawMeta string) (textureMeta, error) {
+func parseRawTextureMeta(rawMeta string) (TextureMeta, error) {
 	splitted := strings.Split(rawMeta, " ")
 	if len(splitted) != 2 {
-		return textureMeta{}, errors.New("invalid raw meta format")
+		return TextureMeta{}, errors.New("invalid raw meta format")
 	}
-	return textureMeta{
-		path: splitted[0],
-		hash: splitted[1],
+	return TextureMeta{
+		Path: splitted[0],
+		Hash: splitted[1],
 	}, nil
 }
 
-func readTextures() ([]textureMeta, error) {
-	file, err := os.Open("textures.txt")
+// Fetch used textures meta list.
+func FetchTextures(fileIndexUrl string) ([]TextureMeta, error) {
+	r, err := http.Get(fileIndexUrl)
 	if err != nil {
-		return nil, fmt.Errorf("open textures.txt: %w", err)
+		return nil, fmt.Errorf("http get (url=%s): %w", fileIndexUrl, err)
 	}
+	if r.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("invalid status code (url=%s) code='%d': %w",
+				fileIndexUrl, r.StatusCode, err)
+	}
+	defer r.Body.Close()
 
-	textures := make([]textureMeta, 0, 4096)
-	scanner := bufio.NewScanner(file)
+	textures := make([]TextureMeta, 0, 4096)
+	scanner := bufio.NewScanner(r.Body)
 	for scanner.Scan() {
 		rawMeta := scanner.Text()
 		meta, err := parseRawTextureMeta(rawMeta)
