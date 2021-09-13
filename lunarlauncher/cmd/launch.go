aa15fbac54f5a35c1054bce3d221b61d2de7dae6
makindotcc <9150636+makindotcc@users.noreply.github.com>
2021-09-13 03:35:45 +0200

    Add messy sandbox-exec rules and run game in sandbox

--- lunarlauncher/cmd/launch.go
+++ lunarlauncher/cmd/launch.go
@@ -3,6 +3,9 @@ package main
 import (
 	"errors"
 	"log"
+	"os"
+	"os/exec"
+	"path"
 
 	"github.com/makindotcc/lunarlauncher"
 )
@@ -37,8 +40,29 @@ func downloadLaunchArtifacts(mcVersion lunarlauncher.McVersion,
 	}
 }
 
+func launchSandboxed(mcVersion lunarlauncher.McVersion) {
+	cmd := exec.Command("sandbox-exec", "-f", "../../../lunar.sb", "java", "--add-modules", "jdk.naming.dns", "--add-exports",
+	"jdk.naming.dns/com.sun.jndi.dns=java.naming", "-Djna.boot.library.path=natives",
+	"--add-opens", "java.base/java.io=ALL-UNNAMED", "-XstartOnFirstThread", "-Xms1024m", "-Xmx1024m", "-Djava.library.path=natives",
+	"-XX:+DisableAttachMechanism", 
+	"-cp", "vpatcher-prod.jar:lunar-prod-optifine.jar:lunar-libs.jar:lunar-assets-prod-1-optifine.jar:"+
+	"lunar-assets-prod-2-optifine.jar:lunar-assets-prod-3-optifine.jar:OptiFine.jar",
+	"com.moonsworth.lunar.patcher.LunarMain", "--version", string(mcVersion), "--accessToken", "0", "--assetIndex", string(mcVersion),
+	"--userProperties", "{}", "--gameDir", ".minecraft", "--texturesDir", "../../textures", "--launcherVersion", "2.7.5",
+	"--width", "1280", "--height", "720")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Dir = path.Join("offline", string(mcVersion))
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	const mcVersion = lunarlauncher.Mc1_16
+
+	log.Println("sandbox:", os.Environ())
 
 	log.Println("Preparing lunar assets...")
 	log.Println("Fetching launch meta...")
@@ -49,6 +73,7 @@ func main() {
 	log.Println("Got launch meta:", launchMeta)
 
 	downloadLaunchTextures(launchMeta)
-
 	downloadLaunchArtifacts(mcVersion, launchMeta.LaunchTypeData.Artifacts)
+
+	launchSandboxed(mcVersion)
 }
